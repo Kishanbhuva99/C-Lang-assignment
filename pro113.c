dcbe1b79bc79468d7d77e1e4168526f22858f4c7
kishanbhuva99 <kishanbhuva72925@gmail.com>
2024-11-10 17:18:00 +0530

    pro 104,96,36,116,113,119 code added

--- pro113.c
+++ pro113.c
@@ -1,7 +1,43 @@
 //Write a program in C to find the maximum number of characters in a string
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
 int main()
 {
-    //baki
+    //baki che -----------------------------------
+    char str[100],maxWord[100];
+    int max=0;
+    char word[100];
+    int j=0;
+    fflush(stdin);
+    printf("Enter string :");
+    gets(str);
+
+    for (int i = 0; str[i] !='\0'; i++)
+    {
+        if (isspace(str[i]))
+        {
+            word[j]='\0';
+            int len=strlen(word);
+            if(len>=max)
+            {
+                strcpy(maxWord,word);
+                printf("%s--max\n",maxWord);
+            }
+            j=0;
+        }
+        else
+        {
+            word[j]=str[i];
+            j++;
+        } 
+    }
+    // word[j]='\0';
+    // int len=strlen(word);
+    //         if(len>max)
+    //         {
+    //             strcpy(maxWord,word);
+    //         }
+    printf("maximum characters --> %s\n",maxWord);
 return 0;
 }
--- pro119.c
+++ pro119.c
@@ -1,1 +1,54 @@
 //Write a program in C to find the largest and smallest words in a string.
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    //baki che-------------------------------------------
+    char str[100],maxWord[100],minword[100];
+    int max=0,min=0;
+    char word[100];
+    int j=0;
+    fflush(stdin);
+    printf("Enter string :");
+    gets(str);
+
+    for (int i = 0; str[i] !='\0'; i++)
+    {
+        if (isspace(str[i]))
+        {
+            word[j]='\0';
+            int len=strlen(word);
+            if(len>=max)
+            {
+                strcpy(maxWord,word);
+                printf("%s--max\n",maxWord);
+            }
+            if(len<=min)
+            {
+                strcpy(minword,word);
+                printf("%s--min\n",minword);
+
+            }
+            j=0;
+        }
+        else
+        {
+            word[j]=str[i];
+            j++;
+        } 
+    }
+    // word[j]='\0';
+    // int len=strlen(word);
+    //         if(len>max)
+    //         {
+    //             strcpy(maxWord,word);
+    //         }
+    //         else if(len<max)
+    //         {
+    //             strcpy(minword,word);
+    //         }
+    printf("Largest Word --> %s\n",maxWord);
+    printf("Smallest Word --> %s\n",minword);
+return 0;
+}
